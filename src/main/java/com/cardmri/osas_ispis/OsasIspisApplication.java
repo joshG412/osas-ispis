8b31b3ad466a72d187343e2bfdfe0bf0789c7e09
osas-ispis <schoolsheets2025@gmail.com>
2025-09-16 04:56:29 -0400

    feat: Initial project structure from Spring Initializr

---
+++ src/main/java/com/cardmri/osas_ispis/OsasIspisApplication.java
@@ -0,0 +1,13 @@
+package com.cardmri.osas_ispis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsasIspisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsasIspisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardmri/osas_ispis/OsasIspisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardmri.osas_ispis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsasIspisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
